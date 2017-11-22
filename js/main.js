e00a440443083188ecf82fa3f311f686ea48845e
Marie-Claire Balabanian <marieclaire.balabanian@gmail.com>
2017-11-22 13:38:49 -0600

    redo

---
+++ js/main.js
@@ -0,0 +1,15 @@
+console.log('this works')
+
+
+// if ($('.image-nav').visible(true)) {
+//     $('#nav').hide()
+// } else {
+//     $('#nav').show()
+// }
+
+
+
+
+if($('.image-nav').is(':visible')){ 
+  $('.nav').hide()
+}
