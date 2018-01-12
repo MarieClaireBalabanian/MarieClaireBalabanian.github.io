5794301689117f6bff78ded3e7196960ba3abb6f
Marie-Claire Balabanian <marieclaire.balabanian@gmail.com>
2018-01-12 17:56:49 -0600

    speck

--- js/main.js
+++ js/main.js
@@ -39,19 +39,26 @@
 //    biggestHeight = $(this).height();
 //  }
 // });
-
+var body = document.body
 // // Set the container height
 // $(".project-panels").height(biggestHeight);
 $('#speck-btn').click(function() {
     $('.speck').css({
         'display': 'block'
-    });    
+    });
+    $('body').css({
+      'overflow': 'hidden'
+    })
+
 });
 
 $('#soundswap-btn').click(function() {
     $('.soundswap').css({
         'display': 'block'
-    });    
+    });
+     $('body').css({
+      'overflow': 'hidden'
+    })    
 });
 
 
@@ -59,18 +66,27 @@ $('#timiseler-btn').click(function() {
     $('.timiseler').css({
         'display': 'block'
     });
+     $('body').css({
+      'overflow': 'hidden'
+    })
 });
 
 $('#wuvr-btn').click(function() {
     $('.wuvr').css({
         'display': 'block'
     });
+     $('body').css({
+      'overflow': 'hidden'
+    })
 });
 
 $('#badnname-btn').click(function() {
     $('.badnname').css({
         'display': 'block'
     });
+     $('body').css({
+      'overflow': 'hidden'
+    })
 });
 
 
@@ -79,6 +95,9 @@ $('.close-soundswap').click(function() {
     $('.soundswap').css({
         'display': 'none'
     });
+    $('body').css({
+      'overflow': 'visible'
+    })
 });
 
 
@@ -86,6 +105,9 @@ $('.close-timiseler').click(function() {
     $('.timiseler').css({
         'display': 'none'
     });
+    $('body').css({
+      'overflow': 'visible'
+    })
 });
 
 
@@ -93,6 +115,9 @@ $('.close-wuvr').click(function() {
     $('.wuvr').css({
         'display': 'none'
     });
+    $('body').css({
+      'overflow': 'visible'
+    })
 });
 
 
@@ -100,10 +125,32 @@ $('.close-badnname').click(function() {
     $('.badnname').css({
         'display': 'none'
     });
+    $('body').css({
+      'overflow': 'visible'
+    })
 });
 
 $('.close-speck').click(function() {
     $('.speck').css({
         'display': 'none'
     });
+    $('body').css({
+      'overflow': 'visible'
+    })
 });
+
+
+
+
+
+
+
+
+
+// var fixed = document.getElementById('fixed');
+
+// fixed.addEventListener('touchmove', function(e) {
+
+//         e.preventDefault();
+
+// }, false);
